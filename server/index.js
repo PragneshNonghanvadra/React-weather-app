8d8f4eaa64d13cb9bcb421f790f9f75b56da647b
PragneshNonghanvadra <pragneshnonghanvadra@gmail.com>
2019-02-16 21:10:03 +0530

    Updated Backend Code

--- server/index.js
+++ server/index.js
@@ -14,6 +14,13 @@ app.use('/api/cities', require('./api/cities'));
 app.use('/api/weather', require('./api/weather'))
 
 
+if(ENV === 'production'){
+  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use((req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  })
+}
+
 app.listen(PORT, () => {
     console.log(`server listening on port ${PORT}...`);
 });
